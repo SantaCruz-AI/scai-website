beb42042bfab8f7164c3b8a875096719c1dd11d8
Brandon Castro <66087694+liljGremlin@users.noreply.github.com>
2023-09-04 23:35:09 -0700

    lol forgot to delete test models

--- components/ScaiLogo.tsx
+++ components/ScaiLogo.tsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree  } from '@react-three/fiber'
 import { MeshPhysicalMaterial } from 'three';
-import * as THREE from "three";
-import { Sky, Torus } from "@react-three/drei"
 import { useGLTF, Stats, OrbitControls, SpotLight } from '@react-three/drei'
 
 
@@ -15,7 +13,7 @@ const Bulb = (props:any) => {
         ref.current.children[2].rotation.y += (delta/4);
     })
 
-    const bulb = useGLTF('/models/SCAILogo8.glb');
+    const bulb = useGLTF('/models/SCAILog.glb');
 
     return (
         <>
@@ -29,9 +27,6 @@ const ScaiLogo = () => {
 
     const [active, activate] = useState(false);
 
-    //<Sky azimuth={0.1} turbidity={5} rayleigh={0.5} inclination={.6} distance={1000} />
-    //<pointLight position={[0, 2, 4]} args={["0xffffff", 3, 0, 1]} />
-    //<ambientLight />
     return (
         <div className="w-[600px] h-[600px] m-auto">
             <Canvas camera={{ position: [0, 0, 6]}} className=' '>
