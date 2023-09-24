961454a3163ecb7bec68a9668e0b2fd001ba38ad
Brandon Castro <66087694+liljGremlin@users.noreply.github.com>
2023-09-23 20:24:21 -0700

    pulled projects.tsx

---
+++ components/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <h1 className="bg-gradient-to-b from-green-200 to-green-500 bg-clip-text text-transparent  text-center mt-10 font-semibold text-6xl ">
+      Projects: TBA
+    </h1>
+  );
+};
+
+export default Projects;
